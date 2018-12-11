478b68a61a4cbcb37a6574f7ef476fc60873e86e
Daniel Otsuka <dkotsuka@gmail.com>
2018-12-11 16:31:38 -0200

    fix image urls

--- js/scripts.js
+++ js/scripts.js
@@ -3,35 +3,35 @@ const projects = [
 		id: 'u-wfe-pj4',
 		name: 'My Reads App',
 		url: 'https://dkotsuka.github.io/my-reads/',
-		img: './img/u-wfe-pj4.jpg',
+		img: './portfolio/img/u-wfe-pj4.jpg',
 		skills: ['react', 'javascript', 'html', 'css']
 	},
 	{
 		id: 'u-wfe-pj3',
 		name: 'Restaurants Reviews',
 		url: 'https://dkotsuka.github.io/mws-restaurant-stage-1/',
-		img: './img/u-wfe-pj3.jpg',
+		img: './portfolio/img/u-wfe-pj3.jpg',
 		tags: ['javascript', 'css', 'html']
 	},
 	{
 		id: 'u-wfe-pj1',
 		name: 'Frog Game Clone',
 		url: 'https://dkotsuka.github.io/front-end-nanodegree-frog-clone/',
-		img: './img/u-wfe-pj1.jpg',
+		img: './portfolio/img/u-wfe-pj1.jpg',
 		tags: ['javascript']
 	},
 	{
 		id: 'pc-js-pj1',
 		name: 'Conversor de Moedas',
 		url: 'https://dkotsuka.github.io/pm-projetos/Projeto012/index.html',
-		img: './img/pm-js-pj1.jpg',
+		img: './portfolio/img/pm-js-pj1.jpg',
 		tags: ['javascript','css']
 	},
 	{
 		id: 'pc-js-pj2',
 		name: 'Calculadora de IMC',
 		url: 'https://dkotsuka.github.io/pm-projetos/Projeto011/index.html',
-		img: './img/pm-js-pj2.jpg',
+		img: './portfolio/img/pm-js-pj2.jpg',
 		tags: ['javascript','css']
 	},
 ];
